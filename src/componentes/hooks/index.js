380114ec81b4579e0a898ebab0588ba78e382063
JGC998 <juangarciacardenas99@gmail.com>
2026-09-07 12:04:41 +0200

    fix: limpiar index.js de exports a módulos eliminados

--- src/componentes/hooks/index.js
+++ src/componentes/hooks/index.js
@@ -2,4 +2,3 @@
 // Uso: import { useGestionCRUD } from '@/componentes/hooks';
 
 export { useGestionCRUD, default } from './useGestionCRUD';
-export { useMessage } from './useMessage';
--- src/componentes/ui/index.js
+++ src/componentes/ui/index.js
@@ -6,13 +6,9 @@ export { default as ContenedorCargando, SpinnerCargando } from './ContenedorCarg
 export { default as ModalConfirmacion, useConfirmacion } from './ModalConfirmacion';
 
 // Componentes existentes
-export { default as TarjetaKPI } from './TarjetaKPI';
 export { default as BarraBusqueda } from './BarraBusqueda';
-export { default as ListaInfo } from './ListaInfo';
 export { default as TablaGenerica } from './TablaGenerica';
-export { default as TablaGestionDatos } from './TablaGestionDatos';
 export { default as Paginacion } from './Paginacion';
 export { default as PaginacionServidor } from './PaginacionServidor';
 export { default as FiltroBusqueda } from './FiltroBusqueda';
 export { default as FiltroEstado } from './FiltroEstado';
-export { default as ProveedorTema } from './ProveedorTema';
